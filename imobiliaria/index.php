e0035ecebc7885a1673cb67202e6e494c1d18e5b
Ricardo Lau <rickgizer@gmail.com>
2016-03-04 15:35:41 -0300

    Alteração da pagina inicial do projeto.

--- imobiliaria/index.php
+++ imobiliaria/index.php
@@ -1,1 +1,11 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Projeto Imobiliaria</title>
+</head>
+<body>
+	<h1>Projeto Imobiliaria</h1>
+	<p>Alterações no projeto.</p>
+</body>
+</html>
